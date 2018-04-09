a80a0c96336b5035066aea4a08d9261099d0b4f7
jose <cardona.josem@gmail.com>
2018-04-08 23:04:51 -0400

    fix mvar tests

--- ioeffect/src/test/scala/scalaz/ioeffect/MVartest.scala
+++ ioeffect/src/test/scala/scalaz/ioeffect/MVartest.scala
@@ -12,26 +12,46 @@ import scala.concurrent.ExecutionContext
 class MVartest(implicit ee: ExecutionEnv)
     extends Specification
     with AroundTimeout {
+  def getEc = ExecutionContext.Implicits.global
+
   def is: SpecStructure =
     s2"""
          MVar Specification
+           have deterministic single take/put behavior        $singleMVarEmptyTest
+           have deterministic single take/put behavior empty  $singleMVarTest
            have deterministic sequential take/put behaviour   $sequentialMVarTest
       """
+
+  def singleMVarEmptyTest = {
+    val ioAction = for {
+      mvar <- MVar.newEmptyMVar[Int](getEc)
+      _ <- mvar.put(1)
+      a <- mvar.read
+      b <- mvar.take
+    } yield (a -> b)
+
+    ioAction.unsafePerformIO() must_== (1 -> 1)
+  }
+
+  def singleMVarTest = {
+    val ioAction = for {
+      mvar <- MVar.newMVar(1)(getEc)
+      a <- mvar.read
+      b <- mvar.take
+    } yield (a -> b)
+
+    ioAction.unsafePerformIO() must_== (1 -> 1)
+  }
 
   def sequentialMVarTest = {
 
     val ioAction = for {
-      ec <- IO.sync(ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(2)))
-      mvar <- MVar.newEmptyMVar[Int](ec)
-      _ <- mvar.put(1)
-      _ <- mvar.put(2)
+      mvar <- MVar.newEmptyMVar[Int](getEc)
+      _ <- mvar.put(1).fork
+      _ <- mvar.put(2).fork
       a <- mvar.take
       b <- mvar.take
-      a <- IO.point(1)
-      b <- IO.point(2)
-      _ <- IO.sync(ec.shutdown())
     } yield (a -> b)
-
 
     ioAction.unsafePerformIO() must_== (1 -> 2)
   }
